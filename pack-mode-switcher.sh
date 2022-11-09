e278c5dfcd61e9f7f8a39e817c3e2b8005921151
Danielle <endergeryt@gmail.com>
2022-11-09 05:42:09 -0800

    Add *nix pack mode switcher (#128)
    * Add *nix pack mode switcher
    
    * Actually implement mode switching properly
    
    * Add instructions for *nix pack mode switcher
    
    * Add flag-based operation
    
    * Quote mode in case

---
+++ pack-mode-switcher.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+set -e
+echo -e "Nomifactory GTCEu Port / Pack mode switcher"
+
+NORMAL_CFG=./config-overrides/normal
+EXPERT_CFG=./config-overrides/expert
+TARGET=./config
+
+if [ -z "$1" ]; then
+  echo -en "Set pack mode (Normal / Expert): "
+  read MODE
+else
+  MODE="$1"
+fi
+
+case "$MODE" in
+  N|n|normal)
+    cp -rf "$NORMAL_CFG"/* "$TARGET"
+    rm -f "$TARGET/globalgamerules.cfg"
+  ;;
+
+  E|e|expert)
+    cp -rf "$EXPERT_CFG"/* "$TARGET"
+  ;;
+
+  *)
+    echo -e "Error: Invalid mode $MODE"
+    exit 1
+  ;;
+esac
+
+echo "Done!"
