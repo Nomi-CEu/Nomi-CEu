c3afe2a4a8b361a7b9f4cabd410f70d5b3205250
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2023-12-03 22:44:23 +1100

    hopefully actually fix crash

--- tools/tasks/misc/gha.ts
+++ tools/tasks/misc/gha.ts
@@ -1,17 +1,18 @@
 import { modpackManifest } from "../../globals";
 import { makeArtifactNameBody } from "../../util/util";
 import sanitize from "sanitize-filename";
+import { setOutput } from "@actions/core";
 
 export async function makeArtifactNames(): Promise<void> {
 	const body = makeArtifactNameBody(modpackManifest.name);
 	const names = {
-		CLIENT: body + "-client",
-		SERVER: body + "-server",
-		LANG: body + "-lang",
-		MMC: body + "-mmc",
+		client: body + "-client",
+		server: body + "-server",
+		lang: body + "-lang",
+		mmc: body + "-mmc",
 	};
 
 	Object.keys(names).forEach((name) => {
-		console.log(`"${name}=${sanitize(names[name].toLowerCase())}" >> $GITHUB_OUTPUT`);
+		setOutput(name, sanitize(names[name].toLowerCase()));
 	});
 }
