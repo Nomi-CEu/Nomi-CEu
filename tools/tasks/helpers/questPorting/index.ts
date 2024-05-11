691e3e236e2d6cedd17e9c0970c822bebe33442e
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-05-11 16:19:04 +1000

    TEMP Test TS Error

--- tools/tasks/helpers/questPorting/index.ts
+++ tools/tasks/helpers/questPorting/index.ts
@@ -9,7 +9,7 @@ import {
 	additions,
 	modifications,
 	removals,
-	setupLogic,
+	setupLog
 } from "./portQBLogic.ts";
 import { setupModifications } from "./portQBModifications.ts";
 import logInfo, { logError } from "../../../utils/log.ts";
