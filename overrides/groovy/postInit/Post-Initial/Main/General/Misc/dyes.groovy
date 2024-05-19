c284b4a1aac3adea644a095a0c8aff9fbd5653cd
Shay <160423885+HandsomeAsianMan@users.noreply.github.com>
2024-05-19 07:22:30 +0100

    Mod Updates As Specified In #472 (#712)
    [EXPAND]
    [[messages]]
    messageTitle = "Update AE2 Unofficial Extended Life (#712)"
    messageBody = '''
    [FEATURE]
    [QOL]
    '''
    
    [[messages]]
    messageTitle = "Update Neeve's AE2 Additions (#712)"
    messageBody = '''
    [FEATURE]
    
    [DETAILS]
    details = ["Adds Many QoL Features", "Adds Larger Cells and Crafting Storage"]
    [DETAILS]
    '''
    [EXPAND]
    
    Co-authored-by: Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
    Co-authored-by: tracer4b <61507029+tracer4b@users.noreply.github.com>

--- overrides/groovy/postInit/Post-Initial/Main/General/Misc/dyes.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Misc/dyes.groovy
@@ -44,29 +44,29 @@ addShapedConversionRecipe(metaitem('dye.white'), item('minecraft:dye', 15))
 
 def dyeHelperMap = [:]
 for (def color : EnumDyeColor.values()) {
-    dyeHelperMap.put(color.getName(), ore('dye' + color.getName().capitalize()))
+	dyeHelperMap.put(color.getName(), ore('dye' + color.getName().capitalize()))
 }
 
 // Fix Elevator Redyeing
 for (def entry in dyeHelperMap.entrySet()) {
-    crafting.remove('elevatorid:redye_' + entry.key)
-    crafting.addShapeless(item('elevatorid:elevator_' + entry.key), [ore('blockElevator'), entry.value])
+	crafting.remove('elevatorid:redye_' + entry.key)
+	crafting.addShapeless(item('elevatorid:elevator_' + entry.key), [ore('blockElevator'), entry.value])
 }
 
 // Fix Slime Block Redyeing
 for (def color : EnumDyeColor.values()) {
-    def name = color.getName()
-    def meta = color.getMetadata()
-    def oreIng = dyeHelperMap.get(name)
-    crafting.remove('darkutils:dyed_slime_block_' + name)
-    crafting.shapedBuilder()
-            .output(item('darkutils:slime_dyed', meta) * 8)
-            .matrix('SSS',
-                    'SDS',
-                    'SSS')
-            .key('S', ore('blockSlime'))
-            .key('D', oreIng)
-            .register()
+	def name = color.getName()
+	def meta = color.getMetadata()
+	def oreIng = dyeHelperMap.get(name)
+	crafting.remove('darkutils:dyed_slime_block_' + name)
+	crafting.shapedBuilder()
+		.output(item('darkutils:slime_dyed', meta) * 8)
+		.matrix('SSS',
+			'SDS',
+			'SSS')
+		.key('S', ore('blockSlime'))
+		.key('D', oreIng)
+		.register()
 }
 
 // Fix Satchel Redeying
@@ -77,29 +77,29 @@ addOreDictToOreDict(ore('dye'), dyeHelperMap.values())
 // https://github.com/GregTechCEu/GregTech/blob/master/src/main/java/gregtech/loaders/recipe/chemistry/ReactorRecipes.java#L619-L626
 // Update when they update.
 for (int i = 0; i < Materials.CHEMICAL_DYES.length; i++) {
-    // Technically the amount being 288 doesn't matter, but calling without amount returns Fluid, and we need FluidStack
-    mods.gregtech.chemical_reactor.removeByOutput(null, [Materials.CHEMICAL_DYES[i].getFluid(288)], null, null)
-    mods.gregtech.chemical_reactor.recipeBuilder()
-            .input(OrePrefix.dye, MarkerMaterials.Color.VALUES[i])
-            .inputs(metaitem('dustSalt') * 2)
-            .fluidInputs(fluid('sulfuric_acid') * 250)
-            .fluidOutputs(Materials.CHEMICAL_DYES[i].getFluid(288))
-            .duration(600).EUt(VA[LV]) // Original is 24, but not in GTValues, so just increase to 30
-            .buildAndRegister()
+	// Technically the amount being 288 doesn't matter, but calling without amount returns Fluid, and we need FluidStack
+	mods.gregtech.chemical_reactor.removeByOutput(null, [Materials.CHEMICAL_DYES[i].getFluid(288)], null, null)
+	mods.gregtech.chemical_reactor.recipeBuilder()
+		.input(OrePrefix.dye, MarkerMaterials.Color.VALUES[i])
+		.inputs(metaitem('dustSalt') * 2)
+		.fluidInputs(fluid('sulfuric_acid') * 250)
+		.fluidOutputs(Materials.CHEMICAL_DYES[i].getFluid(288))
+		.duration(600).EUt(VA[LV]) // Original is 24, but not in GTValues, so just increase to 30
+		.buildAndRegister()
 }
 
 void addOreDictToOreDict(OreDictIngredient addTo, Collection<OreDictIngredient> from) {
-    for (def ing in from) {
-        for (def stack in ing) {
-            addTo.add(stack)
-        }
-    }
+	for (def ing in from) {
+		for (def stack in ing) {
+			addTo.add(stack)
+		}
+	}
 }
 
 void addShapedConversionRecipe(ItemStack outputStack, ItemStack inputStack) {
-    crafting.shapedBuilder()
-            .output(outputStack)
-            .matrix([[inputStack]])
-            .recipeClassFunction((output, width, height, ingredients) -> new ShapedConversionRecipe(output, ingredients, width, height))
-            .register()
+	crafting.shapedBuilder()
+		.output(outputStack)
+		.matrix([[inputStack]])
+		.recipeClassFunction((output, width, height, ingredients) -> new ShapedConversionRecipe(output, ingredients, width, height))
+		.register()
 }
