039df59282e03911bfa9a4fcefa1fe7a6e1b1aa0
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-05-19 16:06:29 +1000

    Formatting

--- overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/nae2.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/nae2.groovy
@@ -1,9 +1,6 @@
-import gregtech.api.recipes.builders.AssemblerRecipeBuilder
-import net.minecraftforge.fluids.FluidStack
-
 import static gregtech.api.GTValues.*
 
-// Removals
+// Crafting Removals
 crafting.removeByOutput(item('nae2:material'))
 crafting.removeByOutput(item('nae2:material:1'))
 crafting.removeByOutput(item('nae2:material:2'))
@@ -17,7 +14,6 @@ crafting.removeByOutput(item('nae2:part'))
 crafting.removeByOutput(item('nae2:reconstruction_chamber'))
 crafting.remove("nae2:block/exposer")
 
-
 /* Item Storage */
 // Void Storage Component
 mods.gregtech.assembler.recipeBuilder()
@@ -28,7 +24,7 @@ mods.gregtech.assembler.recipeBuilder()
 
 // 256k Storage (Tin Recipe is Auto Generated from the Soldering Alloy Recipe)
 mods.gregtech.assembler.recipeBuilder()
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, metaitem('plateTitanium') * 4, ore('circuitIv'), item('appliedenergistics2:material:24'), metaitem('field.generator.lv'), metaitem('wireFineRedAlloy') * 16)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, ore('plateTitanium') * 4, ore('circuitIv'), item('appliedenergistics2:material:24'), metaitem('field.generator.lv'), ore('wireFineRedAlloy') * 16)
 	.fluidInputs(fluid('soldering_alloy') * 72)
 	.outputs(item('nae2:material:1'))
 	.duration(200).EUt(VA[IV])
@@ -36,7 +32,7 @@ mods.gregtech.assembler.recipeBuilder()
 
 // 1024k Storage
 mods.gregtech.assembly_line.recipeBuilder()
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:1'), ore('circuitLuv'), item('appliedenergistics2:material', 24) * 4, ore('plateSignalum') * 4, metaitem('foilPolybenzimidazole') * 4, metaitem('gemExquisiteCertusQuartz'), metaitem('field.generator.mv'), metaitem('wireFineGold') * 16, ore('wireGtSingleEndSteel') * 4)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:1'), ore('circuitLuv'), item('appliedenergistics2:material', 24) * 4, ore('plateSignalum') * 4, ore('foilPolybenzimidazole') * 4, ore('gemExquisiteCertusQuartz'), metaitem('field.generator.mv'), ore('wireFineGold') * 16, ore('wireGtSingleEndSteel') * 4)
 	.fluidInputs(fluid('soldering_alloy') * 576)
 	.outputs(item('nae2:material:2'))
 	.scannerResearch(b -> b.researchStack(item('nae2:material:1')))
@@ -45,7 +41,7 @@ mods.gregtech.assembly_line.recipeBuilder()
 
 // 4096k Storage
 mods.gregtech.assembly_line.recipeBuilder()
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:2'), ore('circuitZpm'), item('appliedenergistics2:material', 24) * 4, ore('plateRuridit') * 4, metaitem('foilPolybenzimidazole') * 4, metaitem('gemExquisiteCertusQuartz'), metaitem('field.generator.hv'), metaitem('wireFineGold') * 32, ore('wireGtSingleEndSteel') * 8)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:2'), ore('circuitZpm'), item('appliedenergistics2:material', 24) * 4, ore('plateRuridit') * 4, ore('foilPolybenzimidazole') * 4, ore('gemExquisiteCertusQuartz'), metaitem('field.generator.hv'), ore('wireFineGold') * 32, ore('wireGtSingleEndSteel') * 8)
 	.fluidInputs(fluid('soldering_alloy') * 576)
 	.outputs(item('nae2:material:3'))
 	.stationResearch(b -> b.researchStack(item('nae2:material:2')).CWUt(16))
@@ -54,7 +50,7 @@ mods.gregtech.assembly_line.recipeBuilder()
 
 // 16384k Storage
 mods.gregtech.assembly_line.recipeBuilder()
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:3'), ore('circuitUv'), item('appliedenergistics2:material', 24) * 4, ore('plateTrinium') * 4, metaitem('foilPolybenzimidazole') * 4, metaitem('gemExquisiteCertusQuartz'), metaitem('field.generator.ev'), metaitem('wireFineGold') * 64, ore('wireGtSingleEndSteel') * 16)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:3'), ore('circuitUv'), item('appliedenergistics2:material', 24) * 4, ore('plateTrinium') * 4, ore('foilPolybenzimidazole') * 4, ore('gemExquisiteCertusQuartz'), metaitem('field.generator.ev'), ore('wireFineGold') * 64, ore('wireGtSingleEndSteel') * 16)
 	.fluidInputs(fluid('soldering_alloy') * 576)
 	.outputs(item('nae2:material:4'))
 	.stationResearch(b -> b.researchStack(item('nae2:material:3')).CWUt(48))
@@ -64,7 +60,7 @@ mods.gregtech.assembly_line.recipeBuilder()
 /* Fluid Storage */
 // 256k Fluid Storage (Tin Recipe is Auto Generated from the Soldering Alloy Recipe)
 mods.gregtech.assembler.recipeBuilder()
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, ore('plateCobaltBrass') * 4, ore('circuitIv'), item('appliedenergistics2:material:22'), metaitem('field.generator.lv'), metaitem('wireFineRedAlloy') * 16)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, ore('plateCobaltBrass') * 4, ore('circuitIv'), item('appliedenergistics2:material:22'), metaitem('field.generator.lv'), ore('wireFineRedAlloy') * 16)
 	.fluidInputs(fluid('soldering_alloy') * 72)
 	.outputs(item('nae2:material:5'))
 	.duration(200).EUt(VA[IV])
@@ -73,7 +69,7 @@ mods.gregtech.assembler.recipeBuilder()
 // 1024k Fluid Storage
 mods.gregtech.assembly_line.recipeBuilder()
 	.outputs(item('nae2:material:6'))
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:5'), ore('circuitLuv'), item('appliedenergistics2:material', 22) * 4, ore('plateWatertightSteel') * 4, metaitem('foilPolybenzimidazole') * 4, metaitem('gemExquisiteCertusQuartz'), metaitem('field.generator.mv'), metaitem('wireFineGold') * 16, ore('wireGtSingleEndSteel') * 4)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:5'), ore('circuitLuv'), item('appliedenergistics2:material', 22) * 4, ore('plateWatertightSteel') * 4, ore('foilPolybenzimidazole') * 4, ore('gemExquisiteCertusQuartz'), metaitem('field.generator.mv'), ore('wireFineGold') * 16, ore('wireGtSingleEndSteel') * 4)
 	.fluidInputs(fluid('soldering_alloy') * 576)
 	.scannerResearch(b -> b.researchStack(item('nae2:material:5')))
 	.duration(400).EUt(VA[LuV])
@@ -82,7 +78,7 @@ mods.gregtech.assembly_line.recipeBuilder()
 // 4096k Fluid Storage
 mods.gregtech.assembly_line.recipeBuilder()
 	.outputs(item('nae2:material:7'))
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:6'), ore('circuitZpm'), item('appliedenergistics2:material', 22) * 4, ore('plateRuridit') * 4, metaitem('foilPolybenzimidazole') * 4, metaitem('gemExquisiteCertusQuartz'), metaitem('field.generator.hv'), metaitem('wireFineGold') * 32, ore('wireGtSingleEndSteel') * 8)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:6'), ore('circuitZpm'), item('appliedenergistics2:material', 22) * 4, ore('plateRuridit') * 4, ore('foilPolybenzimidazole') * 4, ore('gemExquisiteCertusQuartz'), metaitem('field.generator.hv'), ore('wireFineGold') * 32, ore('wireGtSingleEndSteel') * 8)
 	.fluidInputs(fluid('soldering_alloy') * 576)
 	.stationResearch(b -> b.researchStack(item('nae2:material:6')).CWUt(16))
 	.duration(800).EUt(VA[ZPM])
@@ -91,7 +87,7 @@ mods.gregtech.assembly_line.recipeBuilder()
 // 16384k Fluid Storage
 mods.gregtech.assembly_line.recipeBuilder()
 	.outputs(item('nae2:material:8'))
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:7'), ore('circuitUv'), item('appliedenergistics2:material', 22) * 4, ore('plateTrinium') * 4, metaitem('foilPolybenzimidazole') * 4, metaitem('gemExquisiteCertusQuartz'), metaitem('field.generator.ev'), metaitem('wireFineGold') * 64, ore('wireGtSingleEndSteel') * 16)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('nae2:material:7'), ore('circuitUv'), item('appliedenergistics2:material', 22) * 4, ore('plateTrinium') * 4, ore('foilPolybenzimidazole') * 4, ore('gemExquisiteCertusQuartz'), metaitem('field.generator.ev'), ore('wireFineGold') * 64, ore('wireGtSingleEndSteel') * 16)
 	.fluidInputs(fluid('soldering_alloy') * 576)
 	.stationResearch(b -> b.researchStack(item('nae2:material:7')).CWUt(48))
 	.duration(1600).EUt(VA[UV])
@@ -101,21 +97,21 @@ mods.gregtech.assembly_line.recipeBuilder()
 // ME Beam Former
 mods.gregtech.assembler.recipeBuilder()
 	.outputs(item('nae2:part') * 2)
-	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('appliedenergistics2:material', 23) * 2, ore('circuitEv'), metaitem('plateGlowstone') * 4, item('appliedenergistics2:material', 41), item('appliedenergistics2:part', 16) * 4)
+	.inputs(item('appliedenergistics2:quartz_glass') * 4, item('appliedenergistics2:material', 23) * 2, ore('circuitEv'), ore('plateGlowstone') * 4, item('appliedenergistics2:material', 41), item('appliedenergistics2:part', 16) * 4)
 	.duration(200).EUt(VA[EV])
 	.buildAndRegister()
 
 // Reconstruction Chamber
 mods.gregtech.assembler.recipeBuilder()
 	.outputs(item('nae2:reconstruction_chamber'))
-	.inputs(item('actuallyadditions:block_misc', 9), item('appliedenergistics2:quartz_glass') * 4, metaitem('plateBlackSteel') * 4, ore('craftingLensGlass'), ore('circuitHv'), metaitem('sensor.hv'), item('appliedenergistics2:material', 24) * 2)
+	.inputs(item('actuallyadditions:block_misc', 9), item('appliedenergistics2:quartz_glass') * 4, ore('plateBlackSteel') * 4, ore('craftingLensGlass'), ore('circuitHv'), metaitem('sensor.hv'), item('appliedenergistics2:material', 24) * 2)
 	.duration(200).EUt(VA[HV])
 	.buildAndRegister()
 
 // ME Storage Exposer
 mods.gregtech.assembler.recipeBuilder()
 	.outputs(item('nae2:exposer'))
-	.inputs(item('appliedenergistics2:part', 220) * 4, item('appliedenergistics2:material', 47), metaitem('plateStainlessSteel') * 4, ore('circuitHv') * 2, metaitem('emitter.hv'), metaitem('sensor.hv'), item('appliedenergistics2:material', 43) * 2)
+	.inputs(item('appliedenergistics2:part', 220) * 4, item('appliedenergistics2:material', 47), ore('plateStainlessSteel') * 4, ore('circuitHv') * 2, metaitem('emitter.hv'), metaitem('sensor.hv'), item('appliedenergistics2:material', 43) * 2)
 	.duration(200).EUt(VA[HV])
 	.buildAndRegister()
 
