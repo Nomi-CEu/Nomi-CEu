ac55e6a4f33fbdb7b426f6dd1dfe629249e5c1ca
v3ect0rgames <164247766+v3ect0rgames@users.noreply.github.com>
2024-06-22 14:41:03 -0400

    Update midgame.groovy

--- overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
@@ -5,3 +5,11 @@ replaceRecipeInput(metaitem('processing_array'),
 	[[ore('circuitLuv'), item('advsolars:sunnarium'), ore('circuitLuv')],
 	 [metaitem('robot.arm.iv'), metaitem('hull.iv'), metaitem('robot.arm.iv')],
 	 [ore('circuitLuv'), metaitem('tool.datastick'), ore('circuitLuv')]])
+
+// PBI Block into Ingots
+
+recipes.addShapeless(<gregtech:meta_ingot:1010> * 9, [<gregtech:meta_block_compressed_63:2>])
+
+// PBI Ingots into Block
+
+recipes.addShapeless(<gregtech:meta_block_compressed_63:2>, [<gregtech:meta_ingot:1010> * 9>])
