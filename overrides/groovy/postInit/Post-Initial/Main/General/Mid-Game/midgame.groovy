8eed2620b3456fe12b14c1221190d9dfd2fd51d1
v3ect0rgames <164247766+v3ect0rgames@users.noreply.github.com>
2024-06-24 20:05:18 -0400

    Update midgame.groovy

--- overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
@@ -2,16 +2,12 @@ import static com.nomiceu.nomilabs.groovy.GroovyHelpers.RecipeRecyclingHelpers.*
 
 // Processing Array
 replaceRecipeInput(metaitem('processing_array'),
-		[[ore('circuitLuv'), item('advsolars:sunnarium'), ore('circuitLuv')],
-		 [metaitem('robot.arm.iv'), metaitem('hull.iv'), metaitem('robot.arm.iv')],
-		 [ore('circuitLuv'), metaitem('tool.datastick'), ore('circuitLuv')]])
-
-
+        [[ore('circuitLuv'), item('advsolars:sunnarium'), ore('circuitLuv')],
+         [metaitem('robot.arm.iv'), metaitem('hull.iv'), metaitem('robot.arm.iv')],
+         [ore('circuitLuv'), metaitem('tool.datastick'), ore('circuitLuv')]])
 
 // PBI ingots into block
-PBIingot = ore('ingotPolybenzimidazole')
-crafting.addShapeless(metaitem('blockPolybenzimidazole'), PBIingot * 9)
+crafting.addShapeless(metaitem('blockPolybenzimidazole'), [ore('ingotPolybenzimidazole'), ore('ingotPolybenzimidazole'), ore('ingotPolybenzimidazole'), ore('ingotPolybenzimidazole'), ore('ingotPolybenzimidazole'), ore('ingotPolybenzimidazole'), ore('ingotPolybenzimidazole'), ore('ingotPolybenzimidazole')]) 
 
 // PBI block into ingots
- 
-crafting.addShapeless(metaitem('ingotPolybenzimidazole') * 9, ore('blockPolybenzimidazole'))
+crafting.addShapeless([metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole'), metaitem('ingotPolybenzimidazole')], ore('blockPolybenzimidazole'))
