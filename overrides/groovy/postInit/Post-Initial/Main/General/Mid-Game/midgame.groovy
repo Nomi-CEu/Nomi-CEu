c7dd24019c1088cd3d4afd912856200d7faef9ee
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-06-25 22:29:34 +1000

    Formatting

--- overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
@@ -3,8 +3,8 @@ import static com.nomiceu.nomilabs.groovy.GroovyHelpers.RecipeRecyclingHelpers.*
 // Processing Array
 replaceRecipeInput(metaitem('processing_array'),
 	[[ore('circuitLuv'), item('advsolars:sunnarium'), ore('circuitLuv')],
-	[metaitem('robot.arm.iv'), metaitem('hull.iv'), metaitem('robot.arm.iv')],
-	[ore('circuitLuv'), metaitem('tool.datastick'), ore('circuitLuv')]])
+	 [metaitem('robot.arm.iv'), metaitem('hull.iv'), metaitem('robot.arm.iv')],
+	 [ore('circuitLuv'), metaitem('tool.datastick'), ore('circuitLuv')]])
 
 // PBI ingots into block
 crafting.addShapeless(metaitem('blockPolybenzimidazole'), [ore('ingotPolybenzimidazole') * 9])
