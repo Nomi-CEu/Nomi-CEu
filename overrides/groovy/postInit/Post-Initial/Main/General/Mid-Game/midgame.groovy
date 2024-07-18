17655c81cb6c48be0aa0bf42234efdfc410cdda3
ProTriforcer <25221392+ProTriforcer@users.noreply.github.com>
2024-07-18 16:05:18 +0500

    Replace small piles from Rare Earth centrifuging with large piles

--- overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Mid-Game/midgame.groovy
@@ -5,3 +5,17 @@ replaceRecipeInput(metaitem('processing_array'),
 	[[ore('circuitLuv'), item('advsolars:sunnarium'), ore('circuitLuv')],
 	 [metaitem('robot.arm.iv'), metaitem('hull.iv'), metaitem('robot.arm.iv')],
 	 [ore('circuitLuv'), metaitem('tool.datastick'), ore('circuitLuv')]])
+
+// Replace small piles from Rare Earth centrifuging with large piles
+mods.gregtech.centrifuge.removeByInput(20, [metaitem('dustRareEarth')], null)
+mods.gregtech.centrifuge.recipeBuilder()
+    .inputs(metaitem('dustRareEarth'))
+    .chancedOutput(metaitem('dustCadmium'), 625, 100)
+    .chancedOutput(metaitem('dustNeodymium'), 625, 100)
+    .chancedOutput(metaitem('dustSamarium'), 625, 100)
+    .chancedOutput(metaitem('dustCerium'), 625, 100)
+    .chancedOutput(metaitem('dustYttrium'), 625, 100)
+    .chancedOutput(metaitem('dustLanthanum'), 625, 100)
+    .duration(64)
+    .EUt(20)
+    .buildAndRegister()
