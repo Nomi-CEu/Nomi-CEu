Nomi-CEu/Nomi-CEu

commit c807632707562a095e617f3d11d2759dc7d0ed91

Hide Misc Thermal Expansion Arguments (#822)
Fixes #370

[FEATURE]

overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/thermal.groovy
@@ -16,3 +16,13 @@ mods.jei.ingredient.add(item('thermalexpansion:cell').withNbt(['Creative': (byte
 removeAndHideItemIgnoreNBT(item('thermalexpansion:machine', 13)) // Arcane Ensorcellator
 removeAndHideItemIgnoreNBT(item('thermalexpansion:device', 8)) // Insightful Condenser
 removeAndHideItemIgnoreNBT(item('thermalexpansion:device', 9)) // Decoctive Diffuser
+
+// Hide Unused Augments
+mods.hei.removeAndHide(item('thermalexpansion:augment', 352)) // Pyroconvective Loop
+mods.hei.removeAndHide(item('thermalexpansion:augment', 401)) // Flux Reconstruction
+mods.hei.removeAndHide(item('thermalexpansion:augment', 402)) // Parabolic Flux Coupling
+mods.hei.removeAndHide(item('thermalexpansion:augment', 448)) // Reagent Recovery
+mods.hei.removeAndHide(item('thermalexpansion:augment', 656)) // Isentropic Reservoir
+mods.hei.removeAndHide(item('thermalexpansion:augment', 672)) // Closed Loop Cooling
+mods.hei.removeAndHide(item('thermalexpansion:augment', 704)) // Disjunctive Extraction
+

commit f65f64519e5f95edc8284b1cb4c7a6bacc23eff0

Fix GrS Errors Introduced in #822

overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/thermal.groovy
@@ -18,11 +18,11 @@ removeAndHideItemIgnoreNBT(item('thermalexpansion:device', 8)) // Insightful Con
 removeAndHideItemIgnoreNBT(item('thermalexpansion:device', 9)) // Decoctive Diffuser
 
 // Hide Unused Augments
-mods.hei.removeAndHide(item('thermalexpansion:augment', 352)) // Pyroconvective Loop
-mods.hei.removeAndHide(item('thermalexpansion:augment', 401)) // Flux Reconstruction
-mods.hei.removeAndHide(item('thermalexpansion:augment', 402)) // Parabolic Flux Coupling
-mods.hei.removeAndHide(item('thermalexpansion:augment', 448)) // Reagent Recovery
-mods.hei.removeAndHide(item('thermalexpansion:augment', 656)) // Isentropic Reservoir
-mods.hei.removeAndHide(item('thermalexpansion:augment', 672)) // Closed Loop Cooling
-mods.hei.removeAndHide(item('thermalexpansion:augment', 704)) // Disjunctive Extraction
+mods.jei.ingredient.removeAndHide(item('thermalexpansion:augment', 352)) // Pyroconvective Loop
+mods.jei.ingredient.removeAndHide(item('thermalexpansion:augment', 401)) // Flux Reconstruction
+mods.jei.ingredient.removeAndHide(item('thermalexpansion:augment', 402)) // Parabolic Flux Coupling
+mods.jei.ingredient.removeAndHide(item('thermalexpansion:augment', 448)) // Reagent Recovery
+mods.jei.ingredient.removeAndHide(item('thermalexpansion:augment', 656)) // Isentropic Reservoir
+mods.jei.ingredient.removeAndHide(item('thermalexpansion:augment', 672)) // Closed Loop Cooling
+mods.jei.ingredient.removeAndHide(item('thermalexpansion:augment', 704)) // Disjunctive Extraction
 