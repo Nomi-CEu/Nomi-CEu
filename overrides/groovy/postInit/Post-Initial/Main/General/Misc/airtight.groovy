96bcf2c8d8be4289adc46ff2aaa75ce8f2fb4f24
v3ect0rgames <164247766+v3ect0rgames@users.noreply.github.com>
2024-08-24 11:48:56 -0400

    Update airtight.groovy

--- overrides/groovy/postInit/Post-Initial/Main/General/Misc/airtight.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/General/Misc/airtight.groovy
@@ -1,3 +1,9 @@
 import com.nomiceu.nomilabs.util.LabsModeHelper
-
+if (LabsModeHelper.normal) {
+	crafting.addShaped(item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 12, 'lvl': (short) 1]]]), [
+		[item('advancedrocketry:pressuretank')], [item('gregtech:machine', 216)], [item('advancedrocketry:pressuretank')],
+		[item('gregtech:meta_item_1', 144)], [item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 5, 'lvl': (short) 3]]])], [item('gregtech:meta_item_1', 144)],
+		[item('gregtech:fluid_pipe_small', 1012)], [item('gregtech:machine', 276)], [item('gregtech:fluid_pipe_small', 1012)]
+	])
+}
 
