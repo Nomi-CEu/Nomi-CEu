50baf7f6fa1dc1b38877e58a40e861688a35e351
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-08-25 21:22:19 +1000

    Rebalance + Cleanup

--- overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/advancedRocketry.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/advancedRocketry.groovy
@@ -1,17 +1,49 @@
 import com.nomiceu.nomilabs.util.LabsModeHelper
+import net.minecraft.item.ItemStack
+
+import static gregtech.api.GTValues.*
+
+// Add Airtight Seal Recipes
+ItemStack airtight = item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 12, 'lvl': (short) 1]]])
+
 if (LabsModeHelper.normal) {
-	crafting.addShaped(item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 12, 'lvl': (short) 1]]]), [
-		[item('advancedrocketry:pressuretank'), item('gregtech:machine', 216), item('advancedrocketry:pressuretank')],
-		[item('gregtech:meta_item_1', 144), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 5, 'lvl': (short) 3]]]), item('gregtech:meta_item_1', 144)],
-		[item('gregtech:fluid_pipe_small', 1012), item('gregtech:machine', 276), item('gregtech:fluid_pipe_small', 1012)]
-	])
-}
-if (LabsModeHelper.expert) {
-	mods.extendedcrafting.table_crafting.addShaped(2, item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 12, 'lvl': (short) 1]]]), [
-		[item('gregtech:meta_ring', 1068), item('gregtech:meta_item_1', 500), item('gregtech:machine', 217), item('gregtech:meta_item_1', 500), item('gregtech:meta_ring', 1068)],	
-		[item('gregtech:meta_foil', 113), item('advancedrocketry:pressuretank', 1), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 36, 'lvl': (short) 4]]]), item('advancedrocketry:pressuretank', 1), item('gregtech:meta_foil', 113)],
-		[item('gregtech:meta_item_1', 145), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_dust', 360), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_item_1', 145)],
-		[item('gregtech:meta_foil', 113), item('advancedrocketry:pressuretank', 1), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 5, 'lvl': (short) 3]]]), item('advancedrocketry:pressuretank', 1), item('gregtech:meta_foil', 113)],
-		[item('gregtech:meta_ring', 1068), item('gregtech:meta_item_1', 500), item('gregtech:machine', 277), item('gregtech:meta_item_1', 500), item('gregtech:meta_ring', 1068)]
-	])
+	mods.gregtech.assembler.recipeBuilder()
+		.inputs(
+			ore('foilAluminium') * 64,
+			metaitem('duct_tape') * 32,
+			item('nomilabs:cloth') * 16,
+			metaitem('carbon.mesh') * 8,
+			metaitem('fluid.regulator.hv') * 4,
+			metaitem('gas_collector.hv'),
+			item('advancedrocketry:pressuretank', 1), // Normal Pressure Tank
+			item('advancedrocketry:pressuretank', 1),
+			item('advancedrocketry:pressuretank', 1),
+		).fluidInputs(fluid('rubber') * 1296)
+		.outputs(airtight * 4)
+		.duration(500).EUt(VA[HV])
+		.buildAndRegister()
+} else {
+	mods.extendedcrafting.table_crafting.shapedBuilder()
+		.tierAdvanced()
+		.output(airtight * 4)
+		.matrix(
+			'RMCMR',
+			'TPZPT',
+			'FADAF',
+			'TPYPT',
+			'RXBXR',
+		)
+		.key('R', ore('ringRubber'))
+		.key('M', metaitem('carbon.mesh'))
+		.key('C', metaitem('gas_collector.hv'))
+		.key('T', ore('plateDoubleTitanium'))
+		.key('P', item('advancedrocketry:pressuretank', 1)) // Normal Pressure Tank
+		.key('Z', item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 5, 'lvl': (short) 3]]])) // Respiration 3
+		.key('F', metaitem('fluid.regulator.ev'))
+		.key('A', metaitem('pipeNormalFluidPolytetrafluoroethylene'))
+		.key('D', ore('dustQuicklime'))
+		.key('Y', item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 36, 'lvl': (short) 4]]])) // Holding 4
+		.key('B', metaitem('chemical_reactor.hv'))
+		.key('X', metaitem('duct_tape'))
+		.register()
 }
