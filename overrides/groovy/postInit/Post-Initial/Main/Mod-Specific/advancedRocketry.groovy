f5eeff0b183b6a12ff3aa78c1f926dffd65b5345
v3ect0rgames <164247766+v3ect0rgames@users.noreply.github.com>
2024-08-24 23:28:40 -0400

    Update advancedRocketry.groovy

--- overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/advancedRocketry.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/advancedRocketry.groovy
@@ -6,8 +6,8 @@ if (LabsModeHelper.normal) {
 		[item('gregtech:fluid_pipe_small', 1012), item('gregtech:machine', 276), item('gregtech:fluid_pipe_small', 1012)]
 	])
 }
-if (LabsModeHelper.expert) {
-	mods.extendedcrafting.table_crafting.addShaped(1, item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 12, 'lvl': (short) 1]]]), [
+if (LabsModeHelper.normal) {
+	mods.extendedcrafting.table_crafting.addShaped(2, item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 12, 'lvl': (short) 1]]]), [
 		[item('gregtech:meta_ring', 1068), item('gregtech:meta_item_1', 500), item('gregtech:machine', 217), item('gregtech:meta_item_1', 500), item('gregtech:meta_ring', 1068)],	
 		[item('gregtech:meta_foil', 113), item('advancedrocketry:pressuretank', 1), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 36, 'lvl': (short) 4]]]), item('advancedrocketry:pressuretank', 1), item('gregtech:meta_foil', 113)],
 		[item('gregtech:meta_item_1', 145), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_dust', 360), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_item_1', 145)],
@@ -15,4 +15,3 @@ if (LabsModeHelper.expert) {
 		[item('gregtech:meta_ring', 1068), item('gregtech:meta_item_1', 500), item('gregtech:machine', 277), item('gregtech:meta_item_1', 500), item('gregtech:meta_ring', 1068)]
 	])
 }
-
