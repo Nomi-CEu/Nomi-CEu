787d9d6d4efd92f22826c8fb338f91bb2b738506
v3ect0rgames <164247766+v3ect0rgames@users.noreply.github.com>
2024-08-24 23:23:15 -0400

    Update advancedRocketry.groovy

--- overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/advancedRocketry.groovy
+++ overrides/groovy/postInit/Post-Initial/Main/Mod-Specific/advancedRocketry.groovy
@@ -8,10 +8,10 @@ if (LabsModeHelper.normal) {
 }
 if (LabsModeHelper.expert) {
 	mods.extendedcrafting.table_crafting.addShaped(1, item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 12, 'lvl': (short) 1]]]), [
-		[item('gregtech:meta_ring', 1068), item('gregtech:meta_item_1', 500), item('gregtech:machine', 217), item('gregtech:meta_item_1', 500), item('gregtech:meta_ring', 1068)]	
-		[item('gregtech:meta_foil', 113), item('advancedrocketry:pressuretank', 1), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 36, 'lvl': (short) 4]]]), item('advancedrocketry:pressuretank', 1), item('gregtech:meta_foil', 113)]
-		[item('gregtech:meta_item_1', 145), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_dust', 360), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_item_1', 145)]
-		[item('gregtech:meta_foil', 113), item('advancedrocketry:pressuretank', 1), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 5, 'lvl': (short) 3]]]), item('advancedrocketry:pressuretank', 1), item('gregtech:meta_foil', 113)]
+		[item('gregtech:meta_ring', 1068), item('gregtech:meta_item_1', 500), item('gregtech:machine', 217), item('gregtech:meta_item_1', 500), item('gregtech:meta_ring', 1068)],	
+		[item('gregtech:meta_foil', 113), item('advancedrocketry:pressuretank', 1), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 36, 'lvl': (short) 4]]]), item('advancedrocketry:pressuretank', 1), item('gregtech:meta_foil', 113)],
+		[item('gregtech:meta_item_1', 145), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_dust', 360), item('gregtech:fluid_pipe_small', 1016), item('gregtech:meta_item_1', 145)],
+		[item('gregtech:meta_foil', 113), item('advancedrocketry:pressuretank', 1), item('minecraft:enchanted_book').withNbt(['StoredEnchantments': [['id': (short) 5, 'lvl': (short) 3]]]), item('advancedrocketry:pressuretank', 1), item('gregtech:meta_foil', 113)],
 		[item('gregtech:meta_ring', 1068), item('gregtech:meta_item_1', 500), item('gregtech:machine', 277), item('gregtech:meta_item_1', 500), item('gregtech:meta_ring', 1068)]
 	])
 }
