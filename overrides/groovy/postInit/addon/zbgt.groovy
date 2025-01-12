a4249c564134dbb92799ea7a34eb69b13130f6b0
D-Alessian <alessian@alessian.be>
2025-01-12 02:08:21 +0100

    add recipes for Mega multis for ZBGT
    Signed-off-by: D-Alessian <117815201+D-Alessian@users.noreply.github.com>

--- overrides/groovy/postInit/addon/zbgt.groovy
+++ overrides/groovy/postInit/addon/zbgt.groovy
@@ -1,38 +1,57 @@
 package postInit.addon.zbgt
 
+
+
+import com.nomiceu.nomilabs.util.LabsModeHelper
+
 import static gregtech.api.GTValues.*
 
-mods.gregtech.creative_tank_provider.recipeBuilder()
-	.notConsumable(item('nomilabs:creativeportabletankmold'))
-	.inputs(item('minecraft:paper'))
-	.outputs(metaitem('zbgt:creative_computation_provider'))
-	.duration(500)
-	.EUt(100000)
-	.buildAndRegister()
+/*
+Creative item Recipes
+These are recipe for all the ZBGT creative items added
+*/
 
-mods.gregtech.creative_tank_provider.recipeBuilder()
-	.notConsumable(metaitem('infinite_energy'))
-	.inputs(item('extrautils2:trashcanenergy'))
-	.outputs(metaitem('zbgt:creative_energy_sink'))
-	.duration(500)
-	.EUt(100000)
-	.buildAndRegister()
+//NM Specific (using Tank Provider)
+if (LabsModeHelper.normal) {
+	mods.gregtech.creative_tank_provider.recipeBuilder()
+		.notConsumable(item('nomilabs:creativeportabletankmold'))
+		//TODO: Add a creative computation mold item through labs or ZBGT
+		.inputs(item('minecraft:paper'))
+		.outputs(metaitem('zbgt:creative_computation_provider'))
+		.duration(500)
+		.EUt(100000)
+		.buildAndRegister()
 
-mods.gregtech.creative_tank_provider.recipeBuilder()
-	.notConsumable(metaitem('infinite_energy'))
-	.inputs(item('minecraft:redstone_block'))
-	.outputs(metaitem('zbgt:creative_energy_source'))
-	.duration(500)
-	.EUt(100000)
-	.buildAndRegister()
+	mods.gregtech.creative_tank_provider.recipeBuilder()
+		.notConsumable(metaitem('infinite_energy'))
+		.inputs(item('extrautils2:trashcanenergy'))
+		.outputs(metaitem('zbgt:creative_energy_sink'))
+		.duration(500)
+		.EUt(100000)
+		.buildAndRegister()
 
-mods.gregtech.creative_tank_provider.recipeBuilder()
-	.notConsumable(metaitem('infinite_energy'))
-	.inputs(item('minecraft:redstone_block'))
-	.outputs(item('zbgt:creative_heating_coil'))
-	.duration(500)
-	.EUt(100000)
-	.buildAndRegister()
+	mods.gregtech.creative_tank_provider.recipeBuilder()
+		.notConsumable(metaitem('infinite_energy'))
+		.inputs(item('minecraft:redstone_block'))
+		.outputs(metaitem('zbgt:creative_energy_source'))
+		.duration(500)
+		.EUt(100000)
+		.buildAndRegister()
+	//Prevent GT Source dupe
+	crafting.removeByInput(metaitem('zbgt:creative_energy_source'))
+	//Not needed anymore
+	crafting.removeByInput(metaitem('infinite_energy'))
+
+	mods.gregtech.creative_tank_provider.recipeBuilder()
+		.notConsumable(metaitem('infinite_energy'))
+		.inputs(item('minecraft:redstone_block'))
+		.outputs(item('zbgt:creative_heating_coil'))
+		.duration(500)
+		.EUt(100000)
+		.buildAndRegister()
+} else {
+	//TODO: Add HM scripts
+}
 
 mods.gregtech.precise_assembler_recipes.recipeBuilder()
 	.inputs(item('avaritia:resource', 5), item('gregtech:wire_coil', 7) * 512)
@@ -42,3 +61,135 @@ mods.gregtech.precise_assembler_recipes.recipeBuilder()
 	.duration(200).EUt(VA[MAX])
 	.buildAndRegister()
 
+
+/*
+Mega multiblocks Recipes
+These are recipe for all the ZBGT mega multiblocks in a more balanced way.
+Organized by tier in which the script moves them to in NM.
+*/
+
+//IV
+mods.gregtech.assembler.removeByInput(480, [metaitem('cracker') * 64], [fluid('soldering_alloy') * 9216 * 9216])
+mods.gregtech.assembler.removeByInput(480, [metaitem('cracker') * 64], [fluid('tin') * 18432 * 18432])
+mods.gregtech.assembler.recipeBuilder()
+	.inputs(metaitem('cracker') * 64,
+		metaitem('gcym:frameWatertightSteel') * 64,
+		ore('circuitLuv') * 16,
+		item('gcym:unique_casing', 4) * 16,
+		metaitem('electric.pump.iv') * 16,
+		metaitem('pipeNormalFluidPolybenzimidazole') * 16
+	)
+	.fluidInputs(fluid('soldering_alloy') * 9216)
+	.outputs(metaitem('zbgt:mega_ocu'))
+	.duration(1200).EUt(VA[IV])
+	.buildAndRegister()
+
+//LuV
+
+// Mega LCR
+//ZPM
+
+// Mega Electric Blast Furnace
+mods.gregtech.assembler.removeByInput(480, [metaitem('electric_blast_furnace') * 64], [fluid('tin') * 18432 * 18432])
+mods.gregtech.assembler.removeByInput(480, [metaitem('electric_blast_furnace') * 64], [fluid('soldering_alloy') * 9216 * 9216])
+if (LabsModeHelper.normal) {
+	mods.gregtech.assembly_line.recipeBuilder()
+		.inputs(metaitem('electric_blast_furnace') * 64,
+			metaitem('frameNaquadahAlloy') * 64,
+			ore('circuitUv') * 16,
+			metaitem('field.generator.luv') * 16,
+			metaitem('springNaquadahAlloy') * 16,
+			metaitem('plateDenseNaquadahAlloy') * 16,
+			metaitem('wireGtHexUraniumRhodiumDinaquadide') * 16
+		)
+		.fluidInputs(fluid('soldering_alloy') * 9216)
+		.outputs(metaitem('zbgt:mega_ebf'))
+		.stationResearch(b -> b.researchStack(metaitem('gcym:mega_blast_furnace')).CWUt(16))
+		.duration(1200).EUt(VA[LuV])
+		.buildAndRegister()
+} else {
+	mods.gregtech.assembly_line.recipeBuilder()
+		.inputs(metaitem('electric_blast_furnace') * 64,
+			metaitem('frameTritanium') * 64,
+			ore('circuitUhv') * 16,
+			metaitem('field.generator.uv') * 16,
+			metaitem('springTritanium') * 16,
+			metaitem('plateTritanium') * 16,
+			metaitem('wireGtHexUraniumRhodiumDinaquadide') * 16
+		)
+		.fluidInputs(fluid('soldering_alloy') * 9216)
+		.outputs(metaitem('zbgt:mega_ebf'))
+		.stationResearch(b -> b.researchStack(metaitem('gcym:mega_blast_furnace')).CWUt(128).EUt(VA[UHV]))
+		.duration(1200).EUt(VA[UV])
+		.buildAndRegister()
+}
+
+// Mega Vacuum Freezer
+mods.gregtech.assembler.removeByInput(480, [metaitem('vacuum_freezer') * 64], [fluid('tin') * 18432 * 18432])
+mods.gregtech.assembler.removeByInput(480, [metaitem('vacuum_freezer') * 64], [fluid('soldering_alloy') * 9216 * 9216])
+if (LabsModeHelper.normal) {
+	mods.gregtech.assembly_line.recipeBuilder()
+		.inputs(metaitem('vacuum_freezer') * 64,
+			metaitem('frameNaquadahAlloy') * 64,
+			ore('circuitUv') * 16,
+			metaitem('field.generator.luv') * 16,
+			metaitem('pipeNormalFluidNaquadah') * 16,
+			metaitem('plateDenseNaquadahAlloy') * 16,
+			metaitem('wireGtHexUraniumRhodiumDinaquadide') * 16
+		)
+		.fluidInputs(fluid('soldering_alloy') * 9216)
+		.outputs(metaitem('zbgt:mega_vf'))
+		.stationResearch(b -> b.researchStack(metaitem('gcym:mega_vacuum_freezer')).CWUt(16))
+		.duration(1200).EUt(VA[LuV])
+		.buildAndRegister()
+} else {
+	mods.gregtech.assembly_line.recipeBuilder()
+		.inputs(metaitem('vacuum_freezer') * 64,
+			metaitem('frameDuranium') * 64,
+			ore('circuitUhv') * 16,
+			metaitem('field.generator.uv') * 16,
+			metaitem('pipeNormalFluidDuranium') * 16,
+			metaitem('plateDenseTritanium') * 16,
+			metaitem('wireGtHexUraniumRhodiumDinaquadide') * 16
+		)
+		.fluidInputs(fluid('soldering_alloy') * 9216)
+		.outputs(metaitem('zbgt:mega_vf'))
+		.stationResearch(b -> b.researchStack(metaitem('gcym:mega_vacuum_freezer')).CWUt(128).EUt(VA[UHV]))
+		.duration(1200).EUt(VA[UV])
+		.buildAndRegister()
+}
+
+// Mega Alloy Blast Smelter
+mods.gregtech.assembler.removeByInput(480, [metaitem('gcym:alloy_blast_smelter') * 64], [fluid('tin') * 18432 * 18432])
+mods.gregtech.assembler.removeByInput(480, [metaitem('gcym:alloy_blast_smelter') * 64], [fluid('soldering_alloy') * 9216 * 9216])
+if (LabsModeHelper.normal) {
+	mods.gregtech.assembly_line.recipeBuilder()
+		.inputs(metaitem('gcym:alloy_blast_smelter') * 64,
+			metaitem('nomilabs:frameAwakenedDraconium') * 64,
+			ore('circuitUv') * 16,
+			metaitem('field.generator.luv') * 16,
+			metaitem('nomilabs:ringAwakenedDraconium') * 16,
+			metaitem('nomilabs:plateAwakenedDraconium') * 16,
+			metaitem('wireGtHexUraniumRhodiumDinaquadide') * 16
+		)
+		.fluidInputs(fluid('soldering_alloy') * 9216)
+		.outputs(metaitem('zbgt:mega_abs'))
+		.stationResearch(b -> b.researchStack(metaitem('gcym:alloy_blast_smelter')).CWUt(16))
+		.duration(1200).EUt(VA[LuV])
+		.buildAndRegister()
+} else {
+	mods.gregtech.assembly_line.recipeBuilder()
+		.inputs(metaitem('alloy_blast_smelter') * 64,
+			metaitem('nomilabs:frameTaranium') * 64,
+			ore('circuitUhv') * 16,
+			metaitem('field.generator.uv') * 16,
+			metaitem('nomilabs:ringTaranium') * 16,
+			metaitem('nomilabs:plateTaranium') * 16,
+			metaitem('wireGtHexUraniumRhodiumDinaquadide') * 16
+		)
+		.fluidInputs(fluid('soldering_alloy') * 9216)
+		.outputs(metaitem('zbgt:mega_abs'))
+		.stationResearch(b -> b.researchStack(metaitem('gcym:alloy_blast_smelter')).CWUt(128).EUt(VA[UHV]))
+		.duration(1200).EUt(VA[UV])
+		.buildAndRegister()
+}
