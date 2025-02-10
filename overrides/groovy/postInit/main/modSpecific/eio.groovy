da2d27b4b0c49ec0e4e6fd4bc9672f362cc2b0dc
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-02-10 19:03:23 +1100

    Remove EnderIO's 'Old' Glowstone Nano Particles

--- overrides/groovy/postInit/main/modSpecific/eio.groovy
+++ overrides/groovy/postInit/main/modSpecific/eio.groovy
@@ -1,5 +1,6 @@
 package postInit.main.modSpecific
 
+import com.nomiceu.nomilabs.groovy.ChangeRecipeBuilder
 import net.minecraft.item.ItemStack
 import classes.postInit.Common
 
@@ -18,3 +19,12 @@ crafting.shapedBuilder()
 	.key('B', ore('itemConduitBinder'))
 	.key('P', item('appliedenergistics2:part', 36))
 	.replace().register()
+
+// Replace the 'old' Glowstone nano particles with the 'not old' one
+// Not really serving any purpose, but removing the need for two items that do the same thing
+mods.jei.ingredient.hide(item('enderio:block_holy_fog'))
+mods.gregtech.macerator.changeByOutput([item('enderio:block_holy_fog')], null)
+	.forEach { ChangeRecipeBuilder b ->
+		b.changeEachOutput {item('enderio:block_holier_fog') }
+			.replaceAndRegister()
+	}
