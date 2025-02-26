44acd918803ca4478cf2571670f5bdd7f00e27a2
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-02-26 22:11:37 +1100

    Fix Little Tiles Lava and Container Clearing Conflict

--- overrides/groovy/postInit/main/general/misc/fluidCrafting.groovy
+++ overrides/groovy/postInit/main/general/misc/fluidCrafting.groovy
@@ -160,7 +160,7 @@ if (LabsModeHelper.expert) {
 // Little Tiles Lava
 crafting.shapelessBuilder()
 	.output(item('littletiles:ltcoloredblock', 12))
-	.input(fluidIng(fluid('lava')))
+	.input(fluidIng(fluid('lava')), fluidIng(fluid('lava')))
 	.setInputTooltip(IngredientFluidBucket.getInputTooltip(fluid('lava')))
 	.replace().register()
 
