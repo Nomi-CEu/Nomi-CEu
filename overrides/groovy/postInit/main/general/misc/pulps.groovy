5d8902181d180f85e29f9567a9e1d98bfe93c45b
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-07-05 14:00:15 +1000

    Add Extruder Recipes from Polymer Pulps to Rods/Blocks

---
+++ overrides/groovy/postInit/main/general/misc/pulps.groovy
@@ -0,0 +1,43 @@
+import com.nomiceu.nomilabs.groovy.ChangeRecipeBuilder
+import gregtech.api.recipes.ingredients.GTRecipeItemInput
+import gregtech.api.unification.OreDictUnifier
+
+import static gregtech.api.unification.material.Materials.*
+import static gregtech.api.unification.ore.OrePrefix.*
+
+/* Add Extruder Recipes from Pulps to Blocks/Rods */
+
+// Polymer Materials (Skipping Raw Rubber, no blocks or rods)
+var materials = [
+    Rubber,
+	SiliconeRubber,
+	StyreneButadieneRubber,
+	Epoxy,
+	ReinforcedEpoxyResin,
+	Polyethylene,
+	PolyvinylChloride,
+	Polytetrafluoroethylene,
+	Polybenzimidazole,
+	PolyphenyleneSulfide,
+	Polycaprolactam,
+]
+
+var prefixes = [
+    block,
+	stick,
+]
+
+for (def material : materials) {
+	var dust = OreDictUnifier.get(dust, material)
+	for (def prefix : prefixes) {
+		var stack = OreDictUnifier.get(prefix, material)
+		if (stack.empty) continue
+
+		mods.gregtech.extruder.changeByOutput([stack], null)
+			.forEach { ChangeRecipeBuilder builder ->
+				builder.changeInputs {
+					it.set(0, new GTRecipeItemInput(dust))
+				}.buildAndRegister()
+			}
+	}
+}
