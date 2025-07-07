451961f82b325bd4d534d13051467c7a748e3fcc
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-07-07 21:07:37 +1000

    Create Companion GHA for Auto Labs Update

---
+++ tools/tasks/misc/updateLabs.ts
@@ -0,0 +1,37 @@
+import fs from "fs";
+import upath from "upath";
+import { rootDirectory } from "#globals";
+import { ModpackManifest } from "#types/modpackManifest.js";
+
+const labsProjectId = 932060;
+
+export async function updateLabs(): Promise<void> {
+	const id = process.env.ID;
+
+	if (!id) {
+		throw new Error("Labs ID not set.");
+	}
+
+	const numericalId = Number.parseInt(id);
+	if (Number.isNaN(numericalId)) {
+		throw new Error("Labs ID is not a number.");
+	}
+
+	const manifest = JSON.parse(
+		await fs.promises.readFile(
+			upath.join(rootDirectory, "manifest.json"),
+			"utf-8",
+		),
+	) as ModpackManifest;
+
+	for (const file of manifest.files) {
+		if (file.projectID === labsProjectId) {
+			file.fileID = numericalId;
+		}
+	}
+
+	return fs.promises.writeFile(
+		upath.join(rootDirectory, "manifest.json"),
+		JSON.stringify(manifest, null, 2),
+	);
+}
