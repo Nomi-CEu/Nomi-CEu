18e0c4842ca933a660919c245ec3187d596cf08b
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-10-27 21:19:58 +1100

    Fix Blackslashes; Clipboard Quest Reward

---
+++ overrides/groovy/post/classes/Common.groovy
@@ -0,0 +1,68 @@
+package post.classes
+
+import gregtech.api.GTValues
+import net.minecraft.item.ItemStack
+import org.apache.commons.lang3.tuple.Pair
+
+class Common {
+
+    /* Cached Values */
+    private static List<Pair<String, ItemStack>> p2pVariantsCache = null
+    private static List<ItemStack> eioGlassesCache = null
+    private static List<Pair<Integer, String>> voltageNamesCache = null
+
+    /**
+     * Gets a continuous sublist of GregTech tier to voltage names, in lowercase.<br>
+     * <b>Do not modify the returned list!</b>
+     * @param from The start index of the sublist. Inclusive.
+     * @param to The end index of the sublist. Inclusive.
+     */
+    static List<Pair<Integer, String>> getVoltageNames(int from, int to) {
+        if (voltageNamesCache == null) {
+            voltageNamesCache = []
+            for (int i = 0; i < GTValues.VN.length; i++) {
+                voltageNamesCache.add(Pair.of(i, GTValues.VN[i].toLowerCase()))
+            }
+        }
+
+        return voltageNamesCache.subList(from, to + 1)
+    }
+
+    // All P2P Variants, Excluding ME P2P
+    static List<Pair<String, ItemStack>> getP2pVariants() {
+        if (p2pVariantsCache != null) return p2pVariantsCache
+
+        p2pVariantsCache = [
+            Pair.of('Redstone', item('appliedenergistics2:part', 461)),
+            Pair.of('Item', item('appliedenergistics2:part', 462)),
+            Pair.of('Fluid', item('appliedenergistics2:part', 463)),
+            Pair.of('Light', item('appliedenergistics2:part', 467)),
+            Pair.of('RF', item('appliedenergistics2:part', 469)),
+            Pair.of('EU', item('appliedenergistics2:part', 470)),
+            Pair.of('Interface', item('nae2:part', 1)),
+        ]
+        return p2pVariantsCache
+    }
+
+    static List<ItemStack> getEioGlasses() {
+        if (eioGlassesCache != null) return eioGlassesCache
+
+        eioGlassesCache = []
+
+        var prefixes = ['holy', 'unholy', 'pasture', 'not_holy', 'not_unholy', 'not_pasture']
+            .collect { prefix -> "block_${prefix}".toString() }
+        prefixes.add('block')
+
+        var suffixes = ['fused', 'enlightened_fused', 'dark_fused']
+            .collectMany { suffix -> ["${suffix}_quartz", "${suffix}_glass"] }
+
+        for (var prefix : prefixes) {
+            for (var suffix : suffixes) {
+                eioGlassesCache.add(item("enderio:${prefix}_${suffix}"))
+            }
+        }
+
+        return eioGlassesCache
+    }
+
+}
---
+++ overrides/groovy/post/main/mod/jei/ingredients.groovy
@@ -0,0 +1,97 @@
+// SIDE: CLIENT
+// ^, JEI Ingredient Removals affects Client Only
+
+package post.main.mod.jei
+
+import static com.nomiceu.nomilabs.groovy.GroovyHelpers.JEIHelpers.*
+import static gregtech.api.GTValues.*
+
+import com.mcmoddev.densemetals.init.ModBlocks
+import com.nomiceu.nomilabs.util.LabsModeHelper
+import gregtech.common.metatileentities.MetaTileEntities
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraftforge.fluids.FluidUtil
+
+/* General Item Removals. Some item removals may be in other scripts. */
+mods.jei.ingredient.with {
+    // AR
+    hide(item('advancedrocketry:crystal:*')) // Random Crystal Blocks
+    hide(item('advancedrocketry:quartzcrucible'))
+    hide(item('advancedrocketry:iquartzcrucible'))
+
+    hide(item('advancedrocketry:bucketrocketfuel'))
+    hide(item('advancedrocketry:bucketnitrogen'))
+    hide(item('advancedrocketry:buckethydrogen'))
+    hide(item('advancedrocketry:bucketoxygen'))
+    hide(item('advancedrocketry:bucketenrichedlava'))
+
+    // Armor Plus
+    hide(item('armorplus:block_melting_obsidian')) // Null Texture Item
+
+    // Dense Ores
+    var visibleDenseOres = ['iron', 'coal', 'gold', 'diamond', 'emerald', 'lapis', 'redstone']
+
+    // The '.toString()' is important here to normalise the GStrings into Java Strings, to allow for proper set searching
+    var exclusionSet = new ObjectOpenHashSet<>(visibleDenseOres.collect { type ->  "dense_${type}_ore".toString() })
+
+    for (var denseOre : ModBlocks.DENSE_ORES) {
+        if (exclusionSet.contains(denseOre.registryName.path))
+            continue
+
+        if (!denseOre.resolve()) continue // An 'invalid' dense ore
+
+        println "Hiding Dense Ore ${denseOre.registryName}..."
+        removeAndHide(new ItemStack(Item.getItemFromBlock(denseOre)))
+    }
+
+    // NuclearCraft
+    removeAndHide(item('nuclearcraft:block_depleted_uranium'))
+
+    // Nomi Labs
+    if (LabsModeHelper.expert) {
+        hide(item('nomilabs:impossiblerealmdata'))
+    }
+
+    if (LabsModeHelper.normal) {
+        hide(fluid('liquid_nitrogen'))
+    }
+
+    // GregTech
+    // Higher Tier Muffler Hatches
+    for (var tier : [MV, HV, EV, IV, LuV, ZPM, UV]) {
+        removeAndHide(MetaTileEntities.MUFFLER_HATCH[tier].stackForm)
+    }
+
+    // Extended Crafting
+    // Ender & Enhanced Ender
+    hide(item('extendedcrafting:storage', 5)) // Block of Ender
+    hide(item('extendedcrafting:material', 49)) // Enhanced Ender Nugget
+
+    // Better Questing
+    List<ItemStack> lootBoxes = [
+        item('bq_standard:loot_chest'),
+        item('bq_standard:loot_chest', 25),
+        item('bq_standard:loot_chest', 50),
+        item('bq_standard:loot_chest', 75),
+        item('bq_standard:loot_chest', 100),
+        item('bq_standard:loot_chest', 101),
+        item('bq_standard:loot_chest', 102),
+        item('bq_standard:loot_chest', 103),
+        item('bq_standard:loot_chest', 104),
+    ]
+    lootBoxes.forEach { box -> hide(box) }
+
+    hide(item('betterquesting:placeholder'))
+    hide(fluid('betterquesting.placeholder'))
+
+    // Modded Buckets
+    hideItemIgnoreNBT(item('forge:bucketfilled'))
+
+    // Add back Creosote Bucket, has usages in recipes and furnace
+    add(FluidUtil.getFilledBucket(fluid('creosote') * 1000))
+
+    // Add Concrete Cell to JEI
+    add(metaitem('fluid_cell').withNbt(['Fluid' : ['FluidName' : 'concrete', 'Amount' : 1000]]))
+}
